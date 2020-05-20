7d8838cdd30c5308e6816c365f3e15297f24413b
RishabMangal <rishabhmangal1@gmail.com>
2020-05-20 22:06:41 +0530

    handle displayed

--- src/Card.js
+++ src/Card.js
@@ -3,23 +3,23 @@ import './style.css'
 function Card(props) {
     const { name, bio,avatar_url,company,location,login } = props.user;
     return (
-        // <div className={`m-${props.m} card info`}>
         <div className="m-2 card info">
             <div className="card-body row m-0">
                 <div className="col-sm-4">
-                    <img style={{width:"100%"}} src={avatar_url} alt="shakal"></img>
+                    <img style={{ width: "100%" }} src={avatar_url} alt="shakal"></img>
+                    <p className="text-dark text-center" style={{fontFamily:"cursive"}}>@{login}</p>
                 </div>
                 <div className="col-sm-8">
-                    <div className="lead font-weight-bold">
+                    <div className="lead font-weight-bold mt-2">
                         <i className="fas fa-user mx-2"></i>{name ? name : login}
                     </div>
-                    <div className="text-secondary">
+                    <div className="text-secondary mt-2">
                         <i className="fas fa-edit mx-2"></i>{bio ? <p style={{display:"inline",fontFamily:"cursive"}}>{bio}</p> : "No Bio found..."}
                     </div>
-                    <div className="lead">
+                    <div className="lead mt-2">
                         <i className="fas fa-university mx-2"></i>{company ? company : "No Company found..."}
                     </div>
-                    <div className="lead">
+                    <div className="lead mt-2">
                         <i className="fas fa-map-marker-alt mx-2"></i>{location ? location : "No Location found..."}
                     </div>
                 </div>
--- src/Details.js
+++ src/Details.js
@@ -151,7 +151,7 @@ class Details extends Component {
                             </CSSTransition>
                             {
                                 following?(
-                                this.state.following.length ? (this.state.following.map((user, i) => <Card m={0} key={i} user={user}></Card>)) : <p>Loading...</p> ):(<p className="lead text-dark text-center bg-warning">No Following</p>)
+                                this.state.following.length ? (this.state.following.map((user, i) => <Card m={0} key={i} user={user}></Card>)) : null ):(<p className="lead text-dark text-center bg-warning">No Following</p>)
                             }
                         </div>
                     </div>
